9db1263424a888b8fcf34a98885d540b492c1f67
karapetyanm7 <meskarapetyan18@gmail.com>
2025-01-31 23:17:04 +0400

    Initial Commit

--- app/src/main/java/com/example/goaltracker/MainActivity.java
+++ app/src/main/java/com/example/goaltracker/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.goaltracker;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextHabit;
+    private Button buttonAddHabit;
+    private RecyclerView recyclerViewHabits;
+    private com.example.goaltracker.HabitAdapter habitAdapter;
+    private ArrayList<String> habitList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editTextHabit = findViewById(R.id.editTextHabit);
+        buttonAddHabit = findViewById(R.id.buttonAddHabit);
+        recyclerViewHabits = findViewById(R.id.recyclerViewHabits);
+
+        habitList = new ArrayList<>();
+        habitAdapter = new com.example.goaltracker.HabitAdapter(habitList);
+
+        recyclerViewHabits.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewHabits.setAdapter(habitAdapter);
+
+        buttonAddHabit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newHabit = editTextHabit.getText().toString();
+                if (!newHabit.isEmpty()) {
+                    habitList.add(newHabit);
+                    habitAdapter.notifyDataSetChanged();
+                    editTextHabit.setText("");
+                }
+            }
         });
     }
 }
